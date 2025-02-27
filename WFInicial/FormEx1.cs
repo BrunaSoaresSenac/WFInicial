ee9c9a416b53801c4a5df9402b040bf34396d360
BRUNA SOARES PAULINO <bruna.spaulino@senacsp.edu.br>
2025-02-27 19:59:32 -0300

    fix: Corrigido nome de projeto console + WF

--- WFInicial/FormEx1.cs
+++ WFInicial/FormEx1.cs
@@ -3,9 +3,9 @@ using System.Windows.Forms;
 
 namespace WFInicial
 {
-    public partial class Form1 : Form
+    public partial class FormEx1 : Form
     {
-        public Form1()
+        public FormEx1()
         {
             InitializeComponent();
         }
