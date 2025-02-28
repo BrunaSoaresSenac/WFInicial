079d0e4cec253e4acdbf00ab2f6f6b00ca58dbf5
BRUNA SOARES PAULINO <bruna.spaulino@senacsp.edu.br>
2025-02-28 19:10:31 -0300

    feat: Exercicio 3 corrigido no WF

--- WFInicial/FormEx3.Designer.cs
+++ WFInicial/FormEx3.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormEx3";
+            lblNum1 = new Label();
+            txtNum1 = new TextBox();
+            btnMaior = new Button();
+            txtNum2 = new TextBox();
+            lblNum2 = new Label();
+            SuspendLayout();
+            // 
+            // lblNum1
+            // 
+            lblNum1.AutoSize = true;
+            lblNum1.Location = new Point(17, 25);
+            lblNum1.Name = "lblNum1";
+            lblNum1.Size = new Size(63, 15);
+            lblNum1.TabIndex = 0;
+            lblNum1.Text = "Número 1:";
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(85, 22);
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(100, 23);
+            txtNum1.TabIndex = 1;
+            // 
+            // btnMaior
+            // 
+            btnMaior.Location = new Point(36, 90);
+            btnMaior.Name = "btnMaior";
+            btnMaior.Size = new Size(129, 23);
+            btnMaior.TabIndex = 2;
+            btnMaior.Text = "Mostrar Maior";
+            btnMaior.UseVisualStyleBackColor = true;
+            btnMaior.Click += btnMaior_Click;
+            // 
+            // txtNum2
+            // 
+            txtNum2.Location = new Point(85, 51);
+            txtNum2.Name = "txtNum2";
+            txtNum2.Size = new Size(100, 23);
+            txtNum2.TabIndex = 4;
+            // 
+            // lblNum2
+            // 
+            lblNum2.AutoSize = true;
+            lblNum2.Location = new Point(17, 54);
+            lblNum2.Name = "lblNum2";
+            lblNum2.Size = new Size(63, 15);
+            lblNum2.TabIndex = 3;
+            lblNum2.Text = "Número 2:";
+            // 
+            // FormEx3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(197, 153);
+            Controls.Add(txtNum2);
+            Controls.Add(lblNum2);
+            Controls.Add(btnMaior);
+            Controls.Add(txtNum1);
+            Controls.Add(lblNum1);
+            Name = "FormEx3";
+            Text = "FormEx3";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblNum1;
+        private TextBox txtNum1;
+        private Button btnMaior;
+        private TextBox txtNum2;
+        private Label lblNum2;
     }
 }
