dd4e062c6d7736d5d64f90f7887142e96f66cb86
BRUNA SOARES PAULINO <bruna.spaulino@senacsp.edu.br>
2025-02-28 19:56:29 -0300

    feat: Exercicio 5 corrigido no WF

--- WFInicial/FormEx5.Designer.cs
+++ WFInicial/FormEx5.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormEx5";
+            txtNum3 = new TextBox();
+            lblNum3 = new Label();
+            txtNum2 = new TextBox();
+            lblNum2 = new Label();
+            btnMaior = new Button();
+            txtNum1 = new TextBox();
+            lblNum1 = new Label();
+            SuspendLayout();
+            // 
+            // txtNum3
+            // 
+            txtNum3.Location = new Point(105, 80);
+            txtNum3.Name = "txtNum3";
+            txtNum3.Size = new Size(100, 23);
+            txtNum3.TabIndex = 18;
+            // 
+            // lblNum3
+            // 
+            lblNum3.AutoSize = true;
+            lblNum3.Location = new Point(37, 83);
+            lblNum3.Name = "lblNum3";
+            lblNum3.Size = new Size(63, 15);
+            lblNum3.TabIndex = 17;
+            lblNum3.Text = "Número 3:";
+            // 
+            // txtNum2
+            // 
+            txtNum2.Location = new Point(105, 50);
+            txtNum2.Name = "txtNum2";
+            txtNum2.Size = new Size(100, 23);
+            txtNum2.TabIndex = 16;
+            // 
+            // lblNum2
+            // 
+            lblNum2.AutoSize = true;
+            lblNum2.Location = new Point(37, 53);
+            lblNum2.Name = "lblNum2";
+            lblNum2.Size = new Size(63, 15);
+            lblNum2.TabIndex = 15;
+            lblNum2.Text = "Número 2:";
+            // 
+            // btnMaior
+            // 
+            btnMaior.Location = new Point(47, 109);
+            btnMaior.Name = "btnMaior";
+            btnMaior.Size = new Size(148, 23);
+            btnMaior.TabIndex = 14;
+            btnMaior.Text = "Mostrar Maior e Menor";
+            btnMaior.UseVisualStyleBackColor = true;
+            btnMaior.Click += btnMaior_Click;
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(105, 21);
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(100, 23);
+            txtNum1.TabIndex = 13;
+            // 
+            // lblNum1
+            // 
+            lblNum1.AutoSize = true;
+            lblNum1.Location = new Point(37, 24);
+            lblNum1.Name = "lblNum1";
+            lblNum1.Size = new Size(63, 15);
+            lblNum1.TabIndex = 12;
+            lblNum1.Text = "Número 1:";
+            // 
+            // FormEx5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(229, 166);
+            Controls.Add(txtNum3);
+            Controls.Add(lblNum3);
+            Controls.Add(txtNum2);
+            Controls.Add(lblNum2);
+            Controls.Add(btnMaior);
+            Controls.Add(txtNum1);
+            Controls.Add(lblNum1);
+            Name = "FormEx5";
+            Text = "FormEx5";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtNum3;
+        private Label lblNum3;
+        private TextBox txtNum2;
+        private Label lblNum2;
+        private Button btnMaior;
+        private TextBox txtNum1;
+        private Label lblNum1;
     }
 }
--- WFInicial/FormEx5.cs
+++ WFInicial/FormEx5.cs
@@ -16,5 +16,20 @@ namespace WFInicial
         {
             InitializeComponent();
         }
+
+        private void btnMaior_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            int num3 = int.Parse(txtNum3.Text);
+
+            int maior = Math.Max(Math.Max(num1, num2), num3);
+            int menor = Math.Min(Math.Min(num1, num2), num3);
+
+            string mensagem = $"O número {maior} é o maior número.\n" +
+                $"O número {menor} é o menor número.";
+
+            MessageBox.Show(mensagem, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
