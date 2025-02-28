de707df927f549cf875185db387689a82fe6bca7
BRUNA SOARES PAULINO <bruna.spaulino@senacsp.edu.br>
2025-02-28 19:19:23 -0300

    feat: Exercicio 4 corrigido no Console

---
+++ CEx4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CEx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o 1º número:");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("Digite o 2º número:");
+            int num2 = int.Parse(Console.ReadLine()!);
+            Console.Write("Digite o 3º número:");
+            int num3 = int.Parse(Console.ReadLine()!);
+
+            int maior = Math.Max(Math.Max(num1, num2), num3);
+
+            Console.WriteLine($"\nO número {maior} é o maior valor.");
+        }
+    }
+}
